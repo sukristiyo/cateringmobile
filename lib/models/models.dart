ca1f3d61eaab7e24413697a65d9cbc6d570844a4
sukristiyo <49105692+sukristiyo@users.noreply.github.com>
2021-07-04 22:41:30 +0700

    Backend update
    update data food

--- lib/models/models.dart
+++
@@ -1,5 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-part 'food.dart';
-part 'transaction.dart';
-part 'user.dart';
--- lib/shared/shared.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:supercharged/supercharged.dart';
-
-part 'thema.dart';
